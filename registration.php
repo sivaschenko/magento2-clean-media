4f87d4c12c885f90b976b9fcdb0fc09e6ae64d42
Sergey Ivashchenko <serg.ivashchenko@gmail.com>
2018-07-30 15:31:27 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,11 +2,11 @@
 /**
  * @package     Sivaschenko\Media
  * @author      Sergii Ivashchenko <contact@sivaschenko.com>
- * @copyright   2017, Sergii Ivashchenko
+ * @copyright   2017-2018, Sergii Ivashchenko
  * @license     MIT
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Sivaschenko_Media',
+    'Sivaschenko_CleanMedia',
     __DIR__
 );
