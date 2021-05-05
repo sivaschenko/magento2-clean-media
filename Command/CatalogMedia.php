abad42aa08a33cadfc6cf759389a7fae3093bbd5
Martins <martins@saukums.lv>
2021-05-05 20:05:19 +0300

    Move unused files to sub dir

--- Command/CatalogMedia.php
+++ Command/CatalogMedia.php
@@ -5,16 +5,23 @@
  * @copyright   2017-2018, Sergii Ivashchenko
  * @license     MIT
  */
+
 namespace Sivaschenko\CleanMedia\Command;
 
+use FilesystemIterator;
+use Magento\Catalog\Model\ResourceModel\Product\Gallery;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Select;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Driver\File;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use SplFileInfo;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Magento\Framework\App\ResourceConnection;
-use Magento\Catalog\Model\ResourceModel\Product\Gallery;
-use Magento\Framework\Filesystem\Driver\File;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class CatalogMedia extends Command
 {
@@ -22,21 +29,26 @@ class CatalogMedia extends Command
      * Input key for removing unused images
      */
     const INPUT_KEY_REMOVE_UNUSED = 'remove_unused';
-
     /**
      * Inpu tkey for removing orphaned media gallery rows
      */
     const INPUT_KEY_REMOVE_ORPHANED_ROWS = 'remove_orphaned_rows';
-
     /**
      * Input key for listing missing files
      */
     const INPUT_KEY_LIST_MISSING = 'list_missing';
-
     /**
      * Input key for listing unused files
      */
     const INPUT_KEY_LIST_UNUSED = 'list_unused';
+    /**
+     * Input key for moving unused files
+     */
+    const INPUT_KEY_MOVE_UNUSED = 'move_unused';
+    /**
+     *
+     */
+    const DIRECTORY_MOVE_UNUSED = 'unused';
 
     /**
      * @var ResourceConnection
@@ -49,12 +61,18 @@ class CatalogMedia extends Command
     private $file;
 
     /**
-     * Constructor
-     *
-     * @param ResourceConnection $resource
+     * @var Filesystem
      */
-    public function __construct(ResourceConnection $resource, File $file)
+    private $filesystem;
+
+    /**
+     * @param ResourceConnection $resource
+     * @param Filesystem $filesystem
+     * @param File $file
+     */
+    public function __construct(ResourceConnection $resource, Filesystem $filesystem, File $file)
     {
+        $this->filesystem = $filesystem;
         $this->resource = $resource;
         $this->file = $file;
         parent::__construct();
@@ -65,29 +83,34 @@ class CatalogMedia extends Command
      */
     protected function configure()
     {
-        $this->setName('sivaschenko:catalog:media')
-            ->setDescription('Get information about catalog product media')
-            ->addOption(
-                self::INPUT_KEY_REMOVE_UNUSED,
-                'r',
-                InputOption::VALUE_NONE,
-                'Remove unused product images'
-            )->addOption(
-                self::INPUT_KEY_REMOVE_ORPHANED_ROWS,
-                'o',
-                InputOption::VALUE_NONE,
-                'Remove orphaned media gallery rows'
-            )->addOption(
-                self::INPUT_KEY_LIST_MISSING,
-                'm',
-                InputOption::VALUE_NONE,
-                'List missing media files'
-            )->addOption(
-                self::INPUT_KEY_LIST_UNUSED,
-                'u',
-                InputOption::VALUE_NONE,
-                'List unused media files'
-            );
+        $this->setName('sivaschenko:catalog:media')->setDescription(
+            'Get information about catalog product media'
+        )->addOption(
+            self::INPUT_KEY_REMOVE_UNUSED,
+            'r',
+            InputOption::VALUE_NONE,
+            'Remove unused product images'
+        )->addOption(
+            self::INPUT_KEY_REMOVE_ORPHANED_ROWS,
+            'o',
+            InputOption::VALUE_NONE,
+            'Remove orphaned media gallery rows'
+        )->addOption(
+            self::INPUT_KEY_LIST_MISSING,
+            'm',
+            InputOption::VALUE_NONE,
+            'List missing media files'
+        )->addOption(
+            self::INPUT_KEY_LIST_UNUSED,
+            'u',
+            InputOption::VALUE_NONE,
+            'List unused media files'
+        )->addOption(
+            self::INPUT_KEY_MOVE_UNUSED,
+            'd',
+            InputOption::VALUE_NONE,
+            'Move unused image files to the unused folder'
+        );
         parent::configure();
     }
 
@@ -99,10 +122,9 @@ class CatalogMedia extends Command
         $mediaGalleryPaths = $this->getMediaGalleryPaths();
 
         $path = BP . '/pub/media/catalog/product';
-        $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator(
-                $path,
-                \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::FOLLOW_SYMLINKS
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                $path, FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS
             )
         );
 
@@ -113,7 +135,11 @@ class CatalogMedia extends Command
         if ($input->getOption(self::INPUT_KEY_LIST_UNUSED)) {
             $output->writeln('Unused files:');
         }
-        /** @var $info \SplFileInfo */
+        if ($input->getOption(self::INPUT_KEY_MOVE_UNUSED)) {
+            $output->writeln('<info>Moved to the unused folder</info>');
+        }
+
+        /** @var $info SplFileInfo */
         foreach ($iterator as $info) {
             $filePath = str_replace($path, '', $info->getPathname());
             if (strpos($filePath, '/cache') === 0) {
@@ -129,6 +155,17 @@ class CatalogMedia extends Command
                 if ($input->getOption(self::INPUT_KEY_REMOVE_UNUSED)) {
                     unlink($info->getPathname());
                 }
+
+                if ($input->getOption(self::INPUT_KEY_MOVE_UNUSED)) {
+                    $directory = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA);
+                    $unusedPath = str_replace(
+                        $directory->getAbsolutePath(),
+                        $directory->getAbsolutePath() . self::DIRECTORY_MOVE_UNUSED . "/",
+                        $info->getPathname()
+                    );
+
+                    $output->writeln($unusedPath);
+                }
             }
         }
 
@@ -143,7 +180,10 @@ class CatalogMedia extends Command
         }
 
         if ($input->getOption(self::INPUT_KEY_REMOVE_ORPHANED_ROWS)) {
-            $this->resource->getConnection()->delete($this->resource->getTableName(Gallery::GALLERY_TABLE), ['value IN (?)' => $missingFiles]);
+            $this->resource->getConnection()->delete(
+                $this->resource->getTableName(Gallery::GALLERY_TABLE),
+                ['value IN (?)' => $missingFiles]
+            );
         }
 
         $output->writeln(sprintf('Media Gallery entries: %s.', count($mediaGalleryPaths)));
@@ -159,9 +199,9 @@ class CatalogMedia extends Command
     private function getMediaGalleryPaths()
     {
         $connection = $this->resource->getConnection();
-        $select = $connection->select()
-            ->from($this->resource->getTableName(Gallery::GALLERY_TABLE))
-            ->reset(Select::COLUMNS)->columns('value');
+        $select = $connection->select()->from($this->resource->getTableName(Gallery::GALLERY_TABLE))->reset(
+            Select::COLUMNS
+        )->columns('value');
 
         return $connection->fetchCol($select);
     }
