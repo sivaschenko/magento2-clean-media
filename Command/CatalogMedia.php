e25d1bb0590ea736301a7c8a605f15bd19a4fc12
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2021-06-06 22:10:31 +0200

    Added feature to remove duplicated images and update database (#9 by @peterjaap)

--- Command/CatalogMedia.php
+++ Command/CatalogMedia.php
@@ -5,17 +5,23 @@
  * @copyright   2017-2018, Sergii Ivashchenko
  * @license     MIT
  */
+declare(strict_types=1);
+
 namespace Sivaschenko\CleanMedia\Command;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\DB\Select;
+use Magento\Framework\Filesystem;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Catalog\Model\ResourceModel\Product\Gallery;
-use Magento\Framework\Filesystem\Driver\File;
 use Symfony\Component\Console\Input\InputOption;
 
+/**
+ * Class CatalogMedia
+ */
 class CatalogMedia extends Command
 {
     /**
@@ -24,7 +30,7 @@ class CatalogMedia extends Command
     const INPUT_KEY_REMOVE_UNUSED = 'remove_unused';
 
     /**
-     * Inpu tkey for removing orphaned media gallery rows
+     * Input key for removing orphaned media gallery rows
      */
     const INPUT_KEY_REMOVE_ORPHANED_ROWS = 'remove_orphaned_rows';
 
@@ -39,29 +45,40 @@ class CatalogMedia extends Command
     const INPUT_KEY_LIST_UNUSED = 'list_unused';
 
     /**
+     * Input key for listing duplicate files
+     */
+    const INPUT_KEY_LIST_DUPES = 'list_dupes';
+
+    /**
+     * Input key for removing duplicate files and updating database
+     */
+    const INPUT_KEY_REMOVE_DUPES = 'remove_dupes';
+
+    /**
+     * @var Filesystem
+     */
+    public $filesystem;
+
+    /**
      * @var ResourceConnection
      */
     private $resource;
 
     /**
-     * @var File
-     */
-    private $file;
-
-    /**
-     * Constructor
-     *
      * @param ResourceConnection $resource
+     * @param Filesystem $filesystem
      */
-    public function __construct(ResourceConnection $resource, File $file)
-    {
+    public function __construct(
+        ResourceConnection $resource,
+        Filesystem $filesystem
+    ) {
         $this->resource = $resource;
-        $this->file = $file;
+        $this->filesystem = $filesystem;
         parent::__construct();
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     protected function configure()
     {
@@ -78,6 +95,11 @@ class CatalogMedia extends Command
                 InputOption::VALUE_NONE,
                 'Remove orphaned media gallery rows'
             )->addOption(
+                self::INPUT_KEY_REMOVE_DUPES,
+                'x',
+                InputOption::VALUE_NONE,
+                'Remove duplicated files and update database'
+            )->addOption(
                 self::INPUT_KEY_LIST_MISSING,
                 'm',
                 InputOption::VALUE_NONE,
@@ -87,70 +109,117 @@ class CatalogMedia extends Command
                 'u',
                 InputOption::VALUE_NONE,
                 'List unused media files'
+            )->addOption(
+                self::INPUT_KEY_LIST_DUPES,
+                'd',
+                InputOption::VALUE_NONE,
+                'List duplicated files'
             );
         parent::configure();
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $mediaGalleryPaths = $this->getMediaGalleryPaths();
 
-        $path = BP . '/pub/media/catalog/product';
         $iterator = new \RecursiveIteratorIterator(
             new \RecursiveDirectoryIterator(
-                $path,
+                $this->getProductMediaPath(),
                 \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::FOLLOW_SYMLINKS
             )
         );
 
         $files = [];
+        $hashes = [];
         $unusedFiles = 0;
         $cachedFiles = 0;
+        $duplicatedFiles = 0;
+        $removedUnusedFiles = 0;
+        $removedDuplicateFiles = 0;
+        $bytesFreed = 0;
+        $removedRows = 0;
+        $updatedVarcharRows = 0;
+        $updatedGalleryRows = 0;
 
-        if ($input->getOption(self::INPUT_KEY_LIST_UNUSED)) {
-            $output->writeln('Unused files:');
-        }
         /** @var $info \SplFileInfo */
         foreach ($iterator as $info) {
-            $filePath = str_replace($path, '', $info->getPathname());
-            if (strpos($filePath, '/cache') === 0) {
+            $filePath = str_replace($this->getProductMediaPath(), '', $info->getPathname());
+            if (strpos($filePath, '/cache/') === 0) {
                 $cachedFiles++;
                 continue;
             }
             $files[] = $filePath;
+
             if (!in_array($filePath, $mediaGalleryPaths)) {
                 $unusedFiles++;
                 if ($input->getOption(self::INPUT_KEY_LIST_UNUSED)) {
-                    $output->writeln($filePath);
+                    $output->writeln('Unused file: ' . $filePath);
                 }
                 if ($input->getOption(self::INPUT_KEY_REMOVE_UNUSED)) {
-                    unlink($info->getPathname());
+                    $bytesFreed += filesize($info->getPathname());
+                    $removedUnusedFiles += unlink($info->getPathname());
+                    $output->writeln(sprintf('Unused "%s" was removed', $filePath));
+                    continue;
                 }
+            }
+
+            $hash = md5_file($info->getPathname());
+            if (isset($hashes[$hash])) {
+                $duplicatedFiles++;
+                if ($input->getOption(self::INPUT_KEY_LIST_DUPES)) {
+                    $output->writeln(sprintf('Duplicate "%s" to "%s"', $filePath, $hashes[$hash]));
+                }
+                if ($input->getOption(self::INPUT_KEY_REMOVE_DUPES)) {
+                    $bytesFreed += filesize($info->getPathname());
+                    $removedDuplicateFiles += unlink($info->getPathname());
+                    list($updatedVarcharRows, $updatedGalleryRows) = $this->updateDatabaseForRemovedDuplicates($hashes[$hash], $filePath);
+                    $output->writeln(sprintf('Duplicate "%s" was removed', $filePath));
+                }
+            } else {
+                $hashes[$hash] = $filePath;
             }
         }
 
-        if ($input->getOption(self::INPUT_KEY_REMOVE_UNUSED)) {
-            $output->writeln('Unused files were removed!');
-        }
-
         $missingFiles = array_diff($mediaGalleryPaths, $files);
-        if ($input->getOption(self::INPUT_KEY_LIST_MISSING)) {
+        if (!empty($missingFiles) && $input->getOption(self::INPUT_KEY_LIST_MISSING)) {
             $output->writeln('Missing media files:');
             $output->writeln(implode("\n", $missingFiles));
         }
 
         if ($input->getOption(self::INPUT_KEY_REMOVE_ORPHANED_ROWS)) {
-            $this->resource->getConnection()->delete($this->resource->getTableName(Gallery::GALLERY_TABLE), ['value IN (?)' => $missingFiles]);
+            $removedRows += $this->resource->getConnection()->delete(
+                $this->resource->getTableName(Gallery::GALLERY_TABLE),
+                [
+                    'value IN (?)' => $missingFiles
+                ]
+            );
         }
 
+        $output->writeln('');
         $output->writeln(sprintf('Media Gallery entries: %s.', count($mediaGalleryPaths)));
         $output->writeln(sprintf('Files in directory: %s.', count($files)));
         $output->writeln(sprintf('Cached images: %s.', $cachedFiles));
         $output->writeln(sprintf('Unused files: %s.', $unusedFiles));
         $output->writeln(sprintf('Missing files: %s.', count($missingFiles)));
+        $output->writeln(sprintf('Duplicated files: %s.', $duplicatedFiles));
+        $output->writeln('');
+        if ($input->getOption(self::INPUT_KEY_REMOVE_UNUSED)) {
+            $output->writeln(sprintf('Removed unused files: %s.', $removedUnusedFiles));
+        }
+        if ($input->getOption(self::INPUT_KEY_REMOVE_ORPHANED_ROWS)) {
+            $output->writeln(sprintf('Removed orphaned rows: %s.', $removedRows));
+        }
+        if ($input->getOption(self::INPUT_KEY_REMOVE_DUPES)) {
+            $output->writeln(sprintf('Removed duplicated files: %s.', $duplicatedFiles));
+            $output->writeln(sprintf('Updated catalog_product_entity_varchar rows: %s', $updatedVarcharRows));
+            $output->writeln(sprintf('Updated catalog_product_entity_media_gallery rows: %s', $updatedGalleryRows));
+        }
+        if ($input->getOption(self::INPUT_KEY_REMOVE_UNUSED) || $input->getOption(self::INPUT_KEY_REMOVE_DUPES)) {
+            $output->writeln(sprintf('Disk space freed: %s Mb', round($bytesFreed / 1024 / 1024)));
+        }
     }
 
     /**
@@ -164,5 +233,42 @@ class CatalogMedia extends Command
             ->reset(Select::COLUMNS)->columns('value');
 
         return $connection->fetchCol($select);
+    }
+
+    /**
+     * @return string
+     */
+    private function getMediaPath(): string
+    {
+        return $this->filesystem->getDirectoryRead(DirectoryList::MEDIA)->getAbsolutePath();
+    }
+
+    /**
+     * @return string
+     */
+    private function getProductMediaPath(): string
+    {
+        return $this->getMediaPath() . 'catalog/product';
+    }
+
+    /**
+     * @param string $originalPath
+     * @param string $duplicatePath
+     * @return array
+     */
+    private function updateDatabaseForRemovedDuplicates(string $originalPath, string $duplicatePath): array
+    {
+        $connection = $this->resource->getConnection();
+        $resultVarchar = $connection->update(
+            $this->resource->getTableName('catalog_product_entity_varchar'),
+            ['value' => $originalPath],
+            $connection->quoteInto('value = ?', $duplicatePath)
+        );
+        $resultGallery = $connection->update(
+            $this->resource->getTableName('catalog_product_entity_media_gallery'),
+            ['value' => $originalPath],
+            $connection->quoteInto('value = ?', $duplicatePath)
+        );
+        return [$resultVarchar, $resultGallery];
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Overview
 
-The module provides a command for retrieving information about catalog media and allows to remove unused images
+The module provides a command for retrieving information about catalog media files.
+
+```
+bin/magento si:catalog:media
+
+Media Gallery entries: 17996.
+Files in directory: 23717.
+Cached images: 353597.
+Unused files: 5847.
+Missing files: 4.
+Duplicated files: 157.
+```
+
+The following options include more details in the output:
+ - list all unused files with `-u` option
+ - list all files referenced in database but missing in filesystem with `-m` option
+ - list all duplicated files with `-d` option
+
+Also it allows to clean up filesystem and db:
+ - remove unused files with `-r` option
+ - remove database rows referencing non-existing files with `-o` option
+ - remove duplicated files and replace references in database with `-x` option
 
 # Installation
 
@@ -24,6 +45,7 @@ Files in directory: 23717.
 Cached images: 353597.
 Unused files: 5847.
 Missing files: 4.
+Duplicated files: 1.
 ```
 
 ## List missing files
@@ -36,11 +58,13 @@ Missing media files:
 /i/m/image2.jpg
 /i/m/image3.jpg
 /i/m/image4.jpg
+
 Media Gallery entries: 17996.
 Files in directory: 23717.
 Cached images: 353597.
 Unused files: 5847.
 Missing files: 4.
+Duplicated files: 1.
 ```
 
 ## List unused files
@@ -48,20 +72,72 @@ Missing files: 4.
 ```
 bin/magento si:catalog:media -u
 
-Unused files:
-/i/m/image1.jpg
+Unused file: /i/m/image5847.jpg
 ...
-/i/m/image5847.jpg
+
 Media Gallery entries: 17996.
 Files in directory: 23717.
 Cached images: 353597.
 Unused files: 5847.
 Missing files: 4.
+Duplicated files: 1.
 ```
 
 ## Remove unused files
 
 ```
 bin/magento si:catalog:media -r
+
+Unused "/m/i/mixer.glb" was removed
+```
+
+## List duplicated files
+
+```
+bin/magento si:catalog:media -m
+
+Duplicate "/i/m/image5847.jpg" to "/i/m/image5007.jpg"
+
+Media Gallery entries: 17996.
+Files in directory: 23717.
+Cached images: 353597.
+Unused files: 5847.
+Missing files: 4.
+Duplicated files: 1.
+
+Removed unused files: 1.
+Disk space freed: 1 Mb
+```
+
+## Remove duplicated files
+
+```
+bin/magento si:catalog:media -x
+
+Duplicate "/p/u/pub_1.jpg" was removed
+
+Media Gallery entries: 2.
+Files in directory: 4.
+Cached images: 189.
+Unused files: 2.
+Missing files: 0.
+Duplicated files: 1.
+
+Removed duplicated files: 1.
+Updated catalog_product_entity_varchar rows: 1
+Updated catalog_product_entity_media_gallery rows: 1
+Disk space freed: 1 Mb
+```
+
+## List duplicates
+
+```
+bin/magento si:catalog-media:duplicates
+```
+
+## Remove/replace duplicates
+
+```
+bin/magento si:catalog-media:duplicates -r
 ```
 
